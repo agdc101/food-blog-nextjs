cec3d9c6634f6a2d7093cd716652ada4ddabcabf
agdc101 <agdc101@gmail.com>
2024-02-26 19:31:45 +0000

    feat: new about route created

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function AboutPage() {
+    return (
+        <main>
+            <h1>About</h1>
+            <p>This is the about page.</p>
+        </main>
+    );
+}
